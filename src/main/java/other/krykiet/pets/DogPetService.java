51855cc6fd869a0344e916f842518e35a3980ac3
Krykiet <chudzik.kd@gmail.com>
2022-10-21 16:26:39 +0200

    multi profile in single .yml

--- src/main/java/other/krykiet/pets/DogPetService.java
+++ src/main/java/other/krykiet/pets/DogPetService.java
@@ -3,6 +3,6 @@ package other.krykiet.pets;
 public class DogPetService implements PetService {
     @Override
     public String getPetType(){
-        return "Dogs are the best!";
+        return "Dogs are the best!!!";
     }
 }
